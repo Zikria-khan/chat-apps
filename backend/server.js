2806c819491abf689176c1ddafe485cfb1366028
Practice <coding4443@gmail.com>
2024-10-27 03:07:21 -0700

    server chagnes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
@@ -11,49 +10,29 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
-dotenv.config(); 
+dotenv.config();
 
 const app = express(); 
 
 // CORS configuration
 const corsOptions = {
-    origin: 'http://localhost:3000', // Your client URL
+    origin: 'http://localhost:3000', // Your client URL for development
     methods: ["GET", "POST"],
     credentials: true, 
 };
-const __dirname = path.resolve();
+
 // Middleware
 app.use(cors(corsOptions)); 
 app.use(express.json());
 app.use(cookieParser());
 
 // Routes
-
 app.get("/api/test", (req, res) => {
     res.status(200).json({ message: "Success!" });
 });
-app.get("/api",(req,res)=>{
- res.status.json({message:"Success"})   
-})
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-
-// Success route for the root path
-app.get("/", (req, res) => {
-    res.status(200).json({ message: "Success!" });
-});
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-// Redirect all unknown routes to the frontend's index.html
-app.get("*", (_, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"), (err) => {
-        if (err) {
-            console.error("Error serving index.html:", err);
-            res.status(err.status).end();
-        }
-    });
-});
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -90,5 +69,3 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     .catch((error) => {
         console.error("Failed to connect to MongoDB:", error.message);
     });
-
-// No longer exporting the app instance
