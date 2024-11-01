96d5b1600b7015505163426151c5c4de95def54c
Zikria-khan <155012410+Zikria-khan@users.noreply.github.com>
2024-11-01 05:40:30 -0700

    Update SocketContext.jsx

--- mern-chat-app-master/frontend/src/context/SocketContext.jsx
+++ mern-chat-app-master/frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		// Determine the correct URL based on the environment
-		const socketUrl = 'http://chat-apps-gf9k.vercel.app' // Your production server URL
+		const socketUrl = 'https://chat-apps-gf9k.vercel.app' // Your production server URL
 
 		if (authUser) {
 			const socket = io(socketUrl, {
